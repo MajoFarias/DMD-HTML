def7ff8f640501c4a9b69847ed0a78b0e7795c91
Gersain <gersigner@hotmail.com>
2017-04-05 11:07:05 -0500

    first commit gersa

---
+++ colaboraciones-automatizacion.php
@@ -0,0 +1,119 @@
+<?php include('header.php'); ?>
+
+
+<!--∆ eyecatcher In ∆-->
+<section class="headerIn" style="background: url('assets/images/headerIn.jpg') center;">
+	<article class="text containerIn">
+		<h1 class="white" data-scrollreveal="enter up">Solidworks<sup>®</sup></h1>
+	</article>
+</section>
+
+
+<!--∆ breadcrumbs ∆-->
+<section class="containerIn">
+	<div class="row tree">
+		<a href="index.php">Solidworks<sup>®</sup></a><i class="">/</i>
+		<a href="portafolio.php">Renueva tu suscripción</a>
+	</div>
+</section>
+
+<!--∆ second menu ∆-->
+<section class="menuIn margin-top">
+	<ul>
+		<li><a href="">Standard</a></li>
+		<li><a href="">Professional</a></li>
+		<li><a href="">Premium</a></li>
+	</ul>
+</section>
+
+<!--∆ descript ∆-->
+<section class="containerIn content">
+	<h1 class="lineTitle animated fadeIn">Renueva tu Servicio de Suscripción </h1>
+	<h2 class="internSubtitle animated fadeIn">Hay muchas formas en las que DMD puede ayudar a tu empresa, una de ellas es por medio de nuestro
+	Servicio de Suscripción el cual te mantiene actualizado. El servicio de suscripción ofrece una forma
+	inteligente de proteger tu inversión al adquirir SOLIDWORKS. Por medio de nuestro servicio de suscripción
+	Regular, Business o Advanced puedes: 
+	</h2> 
+</section>
+
+<!--∆ main ∆-->
+<section class="suscripcion">
+
+<!-- module One -->
+    <!-- col a -->
+	<article>
+		<img src="assets/images/DMD-colaboraciones-Automatizacion_02.jpg" alt="">
+	</article>
+	<!-- col b -->
+	<article class="bgRed">
+		<h1 class="lineTitle animated suscripTitle fadeIn">Regular</h1>
+		<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio iusto, nisi impedit consequuntur minima, quibusdam velit blanditiis eligendi aliquid omnis facilis, in vitae cumque excepturi. Aut natus, dignissimos quae iste.</p>
+		<a href="#" class="btn btn-primary boton-general" data-toggle="modal" data-target="#myModal">
+    	Renueva
+    	</a>
+	</article>
+
+
+
+		<article>
+			<h1 class="lineTitle animated suscripTitle fadeIn">Solidworks</h1>
+			<ul>
+				<li>Aplicación de Exámenes de evaluación y elaboración de Reportes de diagnóstico.</li>
+				<li>Soporte técnico reactivo sin límite de eventos.</li>
+				<li>Asistencia en tiempo real.</li>
+				<li>Seguimiento preventivo.</li>
+				<li>Reporte.</li>
+				<li>Precios preferenciales.</li>
+				<li>Descuentos y Cortesías.</li>
+				<li>Vinculación.</li>
+			</ul>
+			<a href="#" class="btn btn-primary boton-general" data-toggle="modal" data-target="#myModal">
+        	Renueva
+        	</a>
+
+		</article>
+		<article>
+			<img src="assets/images/suscripcion_05.jpg" alt="">
+		</article>
+		<article>
+			<img src="assets/images/suscripcion_08.jpg" alt="">
+		</article>
+		<article>
+			<h1 class="lineTitle suscripTitle animated fadeIn">Business</h1>
+			<ul>
+				<li>Actualizaciones y mejoras proporcionadas por Dassault Systèmes SolidWorks.</li>
+				<li>Servicios de soporte técnico regular de primer nivel prestados por Diseño y Manufactura Digital.</li>
+				<li>Servicios de soporte técnico excepcional de primer nivel proporcionado por Diseño y Manufactura Digital.</li>
+				<li>Asistencia técnica a través de la web proporcionado por Dassault Systèmes SolidWorks.</li>
+				<li>Privilegios de solicitud de mejoras proporcionado por Dassault Systèmes SolidWorks.</li>
+				<li>Biblioteca de modelos proporcionado por Dassault Systèmes SolidWorks.</li> 
+				<li>Transmisiones web proporcionado por Dassault Systèmes SolidWorks. </li>
+				<li>Manufacturing Network proporcionado por Dassault Systèmes SolidWorks.</li>
+			</ul>
+			<a href="#" class="btn btn-primary boton-general" data-toggle="modal" data-target="#myModal">
+        	Renueva
+        	</a>
+		</article>
+		<article>
+			<h1 class="lineTitle suscripTitle animated fadeIn">Advance</h1>
+			<ul>
+				<li>Actualizaciones y mejoras proporcionadas por Dassault Systèmes SolidWorks.</li>
+				<li>Servicios de soporte técnico regular de primer nivel prestados por Diseño y Manufactura Digital.</li>
+				<li>Servicios de soporte técnico excepcional de primer nivel proporcionado por Diseño y Manufactura Digital.</li>
+				<li>Asistencia técnica a través de la web proporcionado por Dassault Systèmes SolidWorks.</li>
+				<li>Privilegios de solicitud de mejoras proporcionado por Dassault Systèmes SolidWorks.</li>
+				<li>Biblioteca de modelos proporcionado por Dassault Systèmes SolidWorks.</li> 
+				<li>Transmisiones web proporcionado por Dassault Systèmes SolidWorks. </li>
+				
+			</ul>
+        	<a href="#" class="btn btn-primary boton-general" data-toggle="modal" data-target="#myModal">
+        	Renueva
+        	</a>
+		</article>
+		<article>
+			<img src="assets/images/suscripcion_11.jpg" alt="">
+		</article>
+	</section>
+
+<?php include('contactanos.php'); ?>
+<?php include('footer.php'); ?>
---
+++ css/cv.php
@@ -0,0 +1,10 @@
+<!--∆∆∆ HEADER ∆∆∆-->
+<?php include 'header.php';?>
+
+  <!--∆∆ TABS  ∆∆-->
+  <section class="tab container"> 
+    <img class="w90 marginAll" src="assets/images/cv.jpg" alt="">
+  </section>
+
+<!--∆∆∆ FOOTER ∆∆∆-->
+<?php include 'footer.php';?>
